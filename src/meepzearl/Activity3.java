3c9a9a8046f60b2660942a755c6738c8ce97c522
Earlmeepz <earlgimenez21@gmail.com>
2025-08-21 18:07:29 +0800

    Create Activity3.java

---
+++ src/meepzearl/Activity3.java
@@ -0,0 +1,18 @@
+package meepzEarl;
+import java.util.Scanner;
+public class Activity3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scan.nextInt();
+        
+        if(num == 0) {
+            System.out.println("Inputted number is Zero");
+        }else if(num >= 1) {
+            System.out.println(num + " is a positive number");
+        }else{
+            System.out.println(num + " is a negative number");
+        }
+    }
+}
